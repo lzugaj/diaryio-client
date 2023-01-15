bd472d92fbc56d86d877b3937584b95910199974
lzugaj <lzugaj@croz.net>
2023-01-15 17:04:29 +0100

    Setup ChakraUI provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
@@ -9,7 +10,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
